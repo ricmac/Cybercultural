abd0099567f29465e705908e9655605fda47d913
Richard MacManus <ricmac@users.noreply.github.com>
2024-01-24 16:58:23 +0000

    fix

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -13,7 +13,7 @@ module.exports = {
   themeColor: '#B80103', //  Manifest: defines the default theme color for the application
   themeBgColor: '#F3F3F3', // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
   meta_data: {
-    opengraph_default: '/assets/images/33d48ded-f999-4173-8440-9d752446428b_3400x2134.jpg', // fallback/default meta image
+    opengraph_default: '/assets/images/33d48ded-f999-4173-8440-9d752446428b_3400x2134.jpg' // fallback/default meta image
     opengraph_default_alt:
       'Tech history and its impact on our culture. Currently serializing my book, Bubble Blog' // alt text for default meta image
     twitterSite: '', // i.e. @site - twitter profile of the site
