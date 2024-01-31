36c7332eb910272477e3f3008bf05b0311f13355
Richard MacManus <ricmac@users.noreply.github.com>
2024-01-31 10:34:46 +0000

    meta updates

--- _site/src/_data/meta.js
+++ _site/src/_data/meta.js
@@ -1,8 +1,7 @@
 module.exports = {
   url: process.env.URL || 'https://cybercultural.com',
   siteName: 'Cybercultural',
-  siteDescription:
-    'Tech history and its impact on our culture.',
+  siteDescription: 'Tech history and its impact on our culture.',
   siteType: 'Person', // schema
   locale: 'en_EN',
   lang: 'en',
@@ -14,8 +13,7 @@ module.exports = {
   themeBgColor: '#F3F3F3', // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
   meta_data: {
     opengraph_default: '/assets/images/33d48ded-f999-4173-8440-9d752446428b_3400x2134.jpg', // fallback/default meta image
-    opengraph_default_alt:
-      'Tech history and its impact on our culture. Currently serializing my book, Bubble Blog', // alt text for default meta image
+    opengraph_default_alt: 'Tech history and its impact on our culture. Currently serializing my book, Bubble Blog', // alt text for default meta image
     twitterSite: '', // i.e. @site - twitter profile of the site
     twitterCreator: '', // i.e. @author -  twitter profile of the site
     mastodonProfile: 'https://mastodon.social/@ricmac' // i.e. https://front-end.social/@lene - url to your mastodon instance/profile
@@ -23,11 +21,10 @@ module.exports = {
   blog: {
     // this is for the rss feed
     name: 'Cybercultural',
-    description:
-      'Tech history and its impact on our culture.'
+    description: 'Tech history and its impact on our culture.'
   },
   pagination: {
-    itemsPerPage: 20
+    itemsPerPage: 10
   },
   address: {
     // edit all presets or leave empty. They are being used in the pages for privacy.md and imprint.md
