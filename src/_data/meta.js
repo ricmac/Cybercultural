43ea55f67fbc01930130f9c3244cb0db4c303468
Richard MacManus <ricmac@users.noreply.github.com>
2025-05-23 10:52:22 +0100

    updated 11ty images plugin

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -23,7 +23,7 @@ const meta = {
   themeBgColor: '#F3F3F3', // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
   meta_data: {
     opengraph_default: '/assets/images/33d48ded-f999-4173-8440-9d752446428b_3400x2134.jpg', // fallback/default meta image
-    opengraph_default_alt: 'Internet history and its impact on our culture. Currently serializing my book, Bubble Blog', // alt text for default meta image
+    opengraph_default_alt: 'Internet history and its impact on our culture.', // alt text for default meta image
     twitterSite: '', // i.e. @site - twitter profile of the site
     twitterCreator: '', // i.e. @author -  twitter profile of the site
     mastodonProfile: 'https://mastodon.social/@ricmac' // url to your mastodon instance/profile
